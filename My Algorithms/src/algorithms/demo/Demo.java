306885ec879e32d6f70b552b24f477fbd731235d
DKhrakovsky <DKhrakovsky@IL-301396.verint.corp.verintsystems.com>
2016-09-08 22:11:04 +0300

    Demo updated

--- My Algorithms/src/algorithms/demo/Demo.java
+++ My Algorithms/src/algorithms/demo/Demo.java
@@ -1,5 +1,24 @@
 package algorithms.demo;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+import algorithms.mazaGeneratios.GrowingTreeGenerator;
+import algorithms.mazaGeneratios.Maze3d;
+import algorithms.mazaGeneratios.Maze3dGenerator;
+import algorithms.mazaGeneratios.Position;
+import algorithms.search.BFS;
+import algorithms.search.Solution;
+import algorithms.io.MyCompressorOutputStream;
+import algorithms.io.MyDecompressorInputStream;
+import algorithms.search.Solution;
+
+import java.io.OutputStream;
+
 /**
  * Main Demo
  * Creates maze and runs BFS and DFS solutions 
@@ -9,13 +28,6 @@ package algorithms.demo;
  * 
  */
 
-import algorithms.mazaGeneratios.GrowingTreeGenerator;
-import algorithms.mazaGeneratios.Maze3d;
-import algorithms.mazaGeneratios.Maze3dGenerator;
-import algorithms.mazaGeneratios.Position;
-import algorithms.search.BFS;
-import algorithms.search.DFS;
-import algorithms.search.Solution;
 
 public class Demo {
 
@@ -29,10 +41,47 @@ public class Demo {
 		Solution<Position> solution = bfs.search(adapter);
 		System.out.println(solution);
 		System.out.println(bfs.getNumberOfNodesEvaluated());
-		DFS<Position> dfs = new DFS<Position>();
-		Solution<Position> solution2 = dfs.search(adapter);
-		System.out.println(solution2);
-		System.out.println(dfs.getNumberOfNodesEvaluated());
+		
+		// save it to a file
+				OutputStream out;
+				try {
+					out = new MyCompressorOutputStream(
+							new FileOutputStream("1.maz"));
+					byte[] arr = maze.toByteArray();
+					
+					out.write(arr.length);
+					out.write(arr);
+					out.flush();
+					out.close();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}		
+				
+				InputStream in;
+				try {
+					in = new MyDecompressorInputStream(
+						new FileInputStream("1.maz"));
+					int size = in.read();			
+					byte b[]=new byte[size];
+					in.read(b);
+					in.close();	
+					
+					Maze3d loaded = new Maze3d(b);
+					System.out.println("maze loaded from file:");
+					System.out.println(loaded);
+					
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
 	}
 
 }
